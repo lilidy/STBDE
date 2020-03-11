f81479e1fc54b3ee87295af96e19273b3a09dd03
Junior2Kouak <juniorkouakou726@yahoo.com>
2020-03-11 12:19:21 +0100

    team.html>team.php avec barre et footer, inclusion stylesBarres.php pour le style des barres (Over et navbar et footer)

---
+++ SITE/stylesBarres.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/styleAccueil.css">
+<link rel="stylesheet" href="css/styleOverNavbar.css">
+<link rel="stylesheet" href="css/barrePresentation_style.css">
