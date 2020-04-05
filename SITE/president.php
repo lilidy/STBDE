03a42d3bbe8b72bebeabdd13b93e047f8b359a52
Junior2Kouak <juniorkouakou726@yahoo.com>
2020-04-05 20:14:54 +0200

    Mod

--- SITE/president.php
+++ SITE/president.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="fr">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,41 +16,59 @@
 
     <title>President</title>
 
-    </head>
-      <body>
+    <?php
+    
+     $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            
+            //On établit la connexion
+            $stbdeBD = new PDO("mysql:host=$servername;dbname=basebde", $username, $password);
+    
+    $utilisateur = $stbdeBD->query('SELECT * FROM utilisateur');
+        ?>
 
-          <!-- Titre -->
-        <section class="container-fluid text">
-          <div class="textePartenaire">
+</head>
+
+<body>
+
+    <!-- Titre -->
+    <section class="container-fluid text">
+        <div class="textePartenaire">
             <h1>President</h1>
-          </div>
-        </section>
+        </div>
+    </section>
 
-        <!-- Boutton -->
-        <div class="centreur">
-          <select class="custom-select" style="">
-            <option selected class="nomPrenom">Nom Prénom</option>
-            <option value="1">Nom Prénom</option>
-            <option value="2">Nom Prénom</option>
-            <option value="3">Nom Prénom</option>
-          </select>
+    <!-- Boutton -->
+    <div class="centreur">
+        <select class="custom-select" style="">
+            <option selected class="nomPrenom">-- | --</option>
+            <?php    
+                while($donnees = $utilisateur->fetch() ){
+                                
+                echo '<option value="'.$donnees['IdUser'].'">'.$donnees['Nom']." ".$donnees['Prenom'].'</option>';
+            }
+              
+              ?>
+        </select>
 
-          <div class="btn-group" role="group" aria-label="Basic example">
+        <div class="btn-group" role="group" aria-label="Basic example">
             <button type="button" class="btn btn-secondary">Passer le Flambeau</button>
-          </div>
+        </div>
 
-          <div class="btn-group" role="group" aria-label="Basic example">
+        <div class="btn-group" role="group" aria-label="Basic example">
             <button type="button" class="btn btn-secondary">Ajouter au BDE</button>
-          </div>
-
-
         </div>
 
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </div>
 
 
-      </body>
-      </html>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
--- SITE/profil.php
+++ SITE/profil.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html >
+<?php 
+session_start();
+?>
+
+
+<!DOCTYPE html>
 <html lang="fr">
+
 <head>
-  <title>WEBDE Lyon</title>
-  <link rel="stylesheet" href="css/profil_style.css">
+    <title>WEBDE Lyon</title>
+    <link rel="stylesheet" href="css/profil_style.css">
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styleOverNavbar.css">
+
+    <?php
+    
+    include 'lienIconeFooter.html';
+    
+    ?>
 </head>
 
 <body>
+
+    <?php
+        include 'overNavbar.php';
+
+        include 'navbar.php';
+        ?>
+
 
 
     <div class="infos">
@@ -20,20 +40,20 @@
             <div id="div2">
                 <div id="bloc" style="column-count: 2; margin: 0;">
                     <span>Prenom</span>
-                    <input type="text" id="entered"  placeholder="Simon" readonly>
-                </div>    
+                    <input type="text" id="entered" placeholder="<?php echo $_SESSION['prenom'];?>" readonly>
+                </div>
                 <div id="bloc" style="column-count: 2;">
                     <span>Classe</span>
-                    <input type="text" id="entered"  placeholder="B2" readonly>
+                    <input type="text" id="entered" placeholder="<?php echo $_SESSION['niveau'];?>" readonly>
                 </div>
                 <div id="bloc" style="column-count: 2;">
                     <span>Nom</span>
-                    <input type="text" id="entered"  placeholder="PIJARD" readonly>
+                    <input type="text" id="entered" placeholder="<?php echo $_SESSION['nom'];?>" readonly>
                 </div>
-                
+
                 <div id="bloc" style="column-count: 2;">
                     <span>Email</span>
-                    <input type="email" id="entered"  placeholder="simon.pijard@epsi.fr" readonly>
+                    <input type="email" id="entered" placeholder="<?php echo $_SESSION['email'];?>" readonly>
                 </div>
             </div>
 
@@ -44,14 +64,14 @@
         <div class="container-fluid">
             <h4>Nouveau mot de passe</h4>
 
-            
+
             <div id="bloc">
-                <input type="password" id="entered" style="width: 250px;"  placeholder="Nouveau mot de passe">
+                <input type="password" id="entered" style="width: 250px;" placeholder="Nouveau mot de passe">
             </div>
             <div id="bloc">
-                <input type="password" id="entered" style="width: 250px;"  placeholder="Confirmer le mot de passe">
+                <input type="password" id="entered" style="width: 250px;" placeholder="Confirmer le mot de passe">
             </div>
-        <br>
+            <br>
             <input type="submit" value="Valider" class="submit">
         </div>
     </div>
@@ -60,6 +80,12 @@
         <br>
         <a href="acceuil.html"> <small>Supprimer mon compte.</small></a>
     </div>
-    
+
+    <?php
+
+include 'footer.html';
+?>
+
 </body>
+
 </html>
